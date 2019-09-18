05816378fb9e48cf0d3c25745d9988192459fa82
Nothemba Tsheme <tnothemba@gmail.com>
2019-09-18 17:10:51 +0200

    removed unneccessary stuff

--- src/calculator.js
+++ src/calculator.js
@@ -2,33 +2,19 @@ class Calculator{
   constructor(){
      this.slotArr = [];
   }
-
   add(){
       let result = 0;
       for(let i = 0; i < arguments.length; i++){
           result += parseFloat(arguments[i]);
-          this.last = result;
-  }
+      }
       return result;
   }
-
   multiply(){
       let result = 1;
       for(let i = 0; i < arguments.length; i++){
           result *= parseFloat(arguments[i]);
-          this.last = result;
       }
       return result;
-  }
-  Last(){
-      return this.last;
-  }
-  setSlot(number){
-      this.slotArr.push(this.last);
-      return this.slotArr[number - 1];
-  }
-  getSlot(number){
-      return this.slotArr[number - 1];
-  }
-
+    }
 }
+
