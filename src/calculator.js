9a237d7eb39c4d517e569f40768fcf7fce2e0294
Nothemba Tsheme <tnothemba@gmail.com>
2019-09-19 17:00:15 +0200

    cleaned up my code

--- src/calculator.js
+++ src/calculator.js
@@ -1,20 +1,37 @@
 class Calculator{
-  constructor(){
-     this.slotArr = [];
-  }
-  add(){
-      let result = 0;
-      for(let i = 0; i < arguments.length; i++){
-          result += parseFloat(arguments[i]);
-      }
-      return result;
-  }
-  multiply(){
-      let result = 1;
-      for(let i = 0; i < arguments.length; i++){
-          result *= parseFloat(arguments[i]);
-      }
-      return result;
+    constructor(){
+       this.slotArr = [];
     }
-}
-
+    add(){
+        let result = 0;
+        for(let i = 0; i < arguments.length; i++){
+            result += parseFloat(arguments[i]);
+            this.last = result;
+    }
+    return result;
+    }
+  
+    multiply(){
+        let result = 1;
+        for(let i = 0; i < arguments.length; i++){
+            result *= parseFloat(arguments[i]);
+            this.last = result;
+        } 
+        return result;
+          
+    }
+    Last(){
+        return this.last;
+    }
+  
+  
+    setSlot(number){
+        this.slotArr.push(this.last);
+        return this.slotArr[number - 1];
+    }
+    getSlot(number){
+        return this.slotArr[number - 1];
+    }
+  
+  }
+  
